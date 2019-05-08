ee7bc9148c1733d79cf5cbd6d84aab7bb83d548b
Sampath549 <45259400+Sampath549@users.noreply.github.com>
2019-05-08 23:13:44 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('BIGD', 'Big Data / Hadoop', GETDATE())
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('DASC', 'Data Science', GETDATE())
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('PRO', 'Programming', GETDATE())
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('UXUID', 'UX / UI Designing', GETDATE())
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('MOBD', 'Mobile Development', GETDATE())
+INSERT INTO [StepTekDB].[tbl_CourseCategory] VALUES ('DIGIM', 'Digital Marketing', GETDATE())
+
+
+SELECT * FROM [StepTekDB].[tbl_Course] VALUES ('Big Data Architect Masters Course','',GETDATE())
+SELECT * FROM [StepTekDB].[tbl_Course] VALUES ('Big Data Hadoop Certification Training','',GETDATE())
